83758c6c099e9148d109e989c945d61ba830eed2
tebaly <support@freedomsex.net>
2019-11-10 16:27:06 +0300

    init

---
+++ src/worker.js
@@ -0,0 +1,119 @@
+import _ from 'underscore';
+import lscache from 'lscache';
+
+export default class Worker {
+  constructor(config) {
+    // super();
+    this.$api = {};
+    this.$store = {};
+    this.$session = {};
+    this.$modules = {};
+    this.$cache = lscache;
+    this.defaults = {
+      expire: 86400 * 30 * 3,
+    };
+    this.tasks = config.tasks || {};
+    // this.$modules = config.modules;
+    this.loadModules(config.modules);
+    // this.created();
+  }
+
+  api(api) {
+    if (api) {
+      this.$api = api;
+    }
+    return this.$api;
+  }
+
+  store(store) {
+    if (store) {
+      this.$store = store;
+    }
+    return this.$store;
+  }
+
+  // loadModules(modules) {
+  //   _.each(modules, (module, namespace) => {
+  //     _.each(module.tasks, (task, name) => {
+  //       this.tasks[`${namespace}/${name}`] = task;
+  //     });
+  //   });
+  // }
+
+  moduleInfo(name) {
+    const parts = name.split('/');
+    const task = parts.pop();
+    const namespace = parts.join('/');
+    return {namespace, task};
+  }
+
+  loadModules(modules) {
+    _.each(modules, (module, namespace) => {
+      this.$modules[namespace] = module;
+    });
+  }
+
+  saveCache(key, value, expire) {
+    return this.$cache.set(key, value, expire || this.defaults.expire);
+  }
+
+  loadCache(key, defaults) {
+    return this.$cache.get(key) || defaults || null;
+  }
+
+  flush(key, expire, exclude) {
+    const data = this.$store.state[key];
+    this.save(key, data, expire, exclude);
+  }
+
+  load(key, defaults) {
+    const data = this.loadCache(key, defaults);
+    this.$store.commit(key, data);
+  }
+
+  save(key, value, expire, exclude) {
+    this.$store.commit(key, value);
+    let data = value;
+    if (_.isObject(value) && exclude) {
+      data = _.omit(value, exclude);
+    }
+    this.saveCache(key, data, expire);
+  }
+
+
+  session(key) {
+    this.$session[key] = true;
+  }
+
+  context() {
+    return {
+      run: this.run,
+      api: this.$api,
+      store: this.$store,
+      root: this,
+    };
+  }
+
+  isModule(name) {
+    return name.indexOf('/') >= 0;
+  }
+
+  runTask(object, task, args) {
+    return task.call(object, this.context(), args);
+  }
+
+  runModule(name, args) {
+    const {namespace, task} = this.moduleInfo(name);
+    const module = this.$modules[namespace];
+    const target = module.tasks[task];
+    return this.runTask(module, target, args);
+  }
+
+  run(task, args) {
+    if (this.isModule(task)) {
+      return this.runModule(task, args);
+    }
+    const target = this.tasks[task];
+    return this.runTask(this, target, args);
+  }
+}
